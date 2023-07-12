3016c07f253758fd31f4626d90d4506439299950
Vinme <shotiko.akhlouri.1@btu.edu.ge>
2023-07-12 22:15:30 +0400

    first

---
+++ lib.py
@@ -0,0 +1,20 @@
+# 294216883272625420
+class QuadraticEquationSolver:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def solve(self):
+        d = self.b**2 - 4 * self.a * self.c
+
+        if d <= 0:
+            return []
+
+        if d == 0:
+            return [-self.b / (2 * self.a)]
+
+        return [
+            (-self.b + d**0.5) / (2 * self.a),
+            (-self.b - d**0.5) / (2 * self.a),
+        ]
